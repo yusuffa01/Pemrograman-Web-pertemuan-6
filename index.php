6e4f309edacdd7df49947ac13f948bfae9da3cf6
yusuffa01 <80820447+yusuffa01@users.noreply.github.com>
2022-12-12 23:19:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php
+function angkaRomawi($angka)
+{
+	 $angka = intval($angka);
+	 $result = '';
+	 $array = array(
+     'M' => 1000,
+	 'CM' => 900,
+	 'D' => 500,
+	 'CD' => 400,
+	 'C' => 100,
+	 'XC' => 90,
+	 'L' => 50,
+	 'XL' => 40,
+	 'X' => 10,
+	 'IX' => 9,
+	 'V' => 5,
+	 'IV' => 4,
+	 'I' => 1
+    );
+	 
+	 foreach($array as $roman => $value){
+	  $matches = intval($angka/$value);
+	  $result .= str_repeat($roman,$matches);
+	  $angka = $angka % $value;
+	 }
+	 return $result;
+}
+
+?>
